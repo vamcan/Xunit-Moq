328483add09ea99defbd8fdb72eeebc1906be2d7
sp <reza.ghasemi8@gmail.com>
2021-06-19 16:01:04 +0430

    second Commit

---
+++ UnitTest/BankAccount.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImpromptuInterface;
+using Moq;
+using Xunit;
+
+//https://xunit.net/docs/comparisons
+namespace UnitTest
+{
+
+    public interface ILog
+    {
+        void WriteMessage(string msg);
+    }
+
+    public class ConsoleLog : ILog
+    {
+        public void WriteMessage(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+
+    public class Null<T> : DynamicObject where T : class
+    {
+
+        public static T Instance => new Null<T>().ActLike<T>();
+
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object?[]? args, out object? result)
+        {
+            result = default;
+
+            if (typeof(T).GetMethod(binder.Name)?.ReturnType == typeof(void))
+                return true;
+
+            Activator.CreateInstance(typeof(T).GetMethod(binder.Name)?.ReturnType ?? null);
+            return true;
+        }
+    }
+
+    public class BankAccount
+    {
+        public int Balance { get; set; }
+        private readonly ILog _log;
+        public BankAccount(int balance, ILog log)
+        {
+            Balance = balance;
+            _log = log;
+        }
+
+        public void Deposit(int amount)
+        {
+            if (amount < 0)
+                throw new ArgumentException("Deposit Amount Must Be Positive");
+
+            _log.WriteMessage("Depositing...");
+            Balance += amount;
+        }
+
+        public bool Withdraw(int amount)
+        {
+            if (Balance >= amount)
+            {
+                Balance -= amount;
+                return true;
+            }
+
+            return false;
+        }
+
+    }
+
+
+    public class BankAccountTest
+    {
+        private BankAccount ba;
+
+        public BankAccountTest()
+        {
+            var log = new Mock<ILog>();
+            ba = new BankAccount(50, log.Object);
+        }
+
+
+
+        [Fact]
+
+        public void BankAccountShouldIncreaseOnPositiveDeposit()
+        {
+
+            ba.Deposit(100);
+
+            Assert.Equal(150, ba.Balance);
+        }
+
+       
+
+
+        [Fact]
+        public void BankAccountShouldThrowUnPositiveAmount()
+        {
+
+            var ex = Assert.Throws<ArgumentException>((() => ba.Deposit(-1)));
+            Assert.StartsWith("Deposit Amount Must Be Positive", ex.Message);
+        }
+
+        [Theory]
+        [InlineData(10, true, 40)]
+        [InlineData(100, false, 50)]
+        public void TestMultipleWithdrawScenario(int amount, bool shouldSucceed, int expectedBalance)
+        {
+            var result = ba.Withdraw(amount);
+
+            Assert.Equal(shouldSucceed, result);
+            Assert.Equal(expectedBalance, ba.Balance);
+        }
+
+        [Fact]
+        public void DepositIntegrationTest()
+        {
+            ba.Deposit(100);
+
+            Assert.Equal(150,ba.Balance);
+        }
+
+        [Fact]
+        public void Test()
+        {
+            // var _actualFuel = 2 + 2;
+            //   Assert.Inconclusive("The Result is not Set");
+            // Assert.Warn("The Result is not Set");
+            //   Assert.AreEqual(a,4);
+            // Assert.That(_actualFuel, Is.EqualTo(4));
+
+            //Assert.That(28, Is.EqualTo(_actualFuel)); // Tests whether the specified values are equal. 
+            //Assert.That(28, Is.Not.EqualTo(_actualFuel)); // Tests whether the specified values are unequal. Same as AreEqual for numeric values.
+            //Assert.That(_expectedRocket, Is.SameAs(_actualRocket)); // Tests whether the specified objects both refer to the same object
+            //Assert.That(_expectedRocket, Is.Not.SameAs(_actualRocket)); // Tests whether the specified objects refer to different objects
+            //Assert.That(_isThereEnoughFuel, Is.True); // Tests whether the specified condition is true
+            //Assert.That(_isThereEnoughFuel, Is.False); // Tests whether the specified condition is false
+            //Assert.That(_actualRocket, Is.Null); // Tests whether the specified object is null
+            //Assert.That(_actualRocket, Is.Not.Null); // Tests whether the specified object is non-null
+            //Assert.That(_actualRocket, Is.InstanceOf<Falcon9Rocket>()); // Tests whether the specified object is an instance of the expected type
+            //Assert.That(_actualRocket, Is.Not.InstanceOf<Falcon9Rocket>()); // Tests whether the specified object is not an instance of type
+            //Assert.That(_actualFuel, Is.GreaterThan(20)); // Tests whether the specified object greater than the specified value
+            //Assert.That(28, Is.EqualTo(_actualFuel).Within(0.50));
+            //// Tests whether the specified values are nearly equal within the specified tolerance.
+            //Assert.That(28, Is.EqualTo(_actualFuel).Within(2).Percent);
+            //// Tests whether the specified values are nearly equal within the specified % tolerance.
+            //Assert.That(_actualRocketParts, Has.Exactly(10).Items);
+            //// Tests whether the specified collection has exactly the stated number of items in it.
+            //Assert.That(_actualRocketParts, Is.Unique);
+            //// Tests whether the items in the specified collections are unique.
+            //Assert.That(_actualRocketParts, Does.Contain(_expectedRocketPart));
+            //// Tests whether a given items is present in the specified list of items.
+            //Assert.That(_actualRocketParts, Has.Exactly(1).Matches<RocketPart>(part => part.Name == "Door" && part.Height == "200"));
+            //// Tests whether the specified collection has exactly the stated item in it.
+
+
+            //StringAssert.AreEqualIgnoringCase(_expectedBellatrixTitle, "Bellatrix"); // Tests whether the specified strings are equal ignoring their casing
+            //StringAssert.Contains(_expectedBellatrixTitle, "Bellatrix"); // Tests whether the specified string contains the specified substring
+            //StringAssert.DoesNotContain(_expectedBellatrixTitle, "Bellatrix"); // Tests whether the specified string doesn't contain the specified substring
+            //StringAssert.StartsWith(_expectedBellatrixTitle, "Bellatrix"); // Tests whether the specified string begins with the specified substring
+            //StringAssert.StartsWith(_expectedBellatrixTitle, "Bellatrix"); // Tests whether the specified string begins with the specified substring
+            //StringAssert.IsMatch("(281)388-0388", @"(?d{3})?-? *d{3}-? *-?d{4}"); // Tests whether the specified string matches a regular expression
+            //StringAssert.DoesNotMatch("281)388-0388", @"(?d{3})?-? *d{3}-? *-?d{4}"); // Tests whether the specified string does not match a regular expression
+
+        }
+
+    }
+}
---
+++ UnitTest/MockTest.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using Moq;
+using Moq.Protected;
+using Xunit;
+using Range = Moq.Range;
+
+namespace UnitTest
+{
+    public interface IBaz
+    {
+        string Name { get; set; }
+    }
+
+    public class Consumer
+    {
+        private readonly IFoo _foo;
+
+        public Consumer(IFoo foo)
+        {
+            _foo = foo;
+        }
+
+        public void Hello()
+        {
+            _foo.DoSomething("ping");
+            var name = _foo.Name;
+            _foo.SomeOtherProperty = 123;
+        }
+    }
+
+    public abstract class Person
+    {
+        protected int SSN { get; set; }
+
+        protected abstract void Execute(string cmd);
+    }
+
+    public delegate void AlienAbduction(int galaxy);
+
+    public interface IAnimal
+    {
+        event EventHandler FallsIll;
+        void Stumble();
+
+        event AlienAbduction AlienAbduction;
+    }
+
+    public class Doctor
+    {
+        public int TimesCured { get; set; }
+        public int AbductionObserved { get; set; }
+        public Doctor(IAnimal animal)
+        {
+            animal.FallsIll += (sender, args) =>
+            {
+                TimesCured++;
+                Console.WriteLine("I will Cure You");
+            };
+
+            animal.AlienAbduction += galaxy => ++AbductionObserved;
+        }
+
+
+    }
+
+    public interface IFoo
+    {
+        bool DoSomething(string value);
+        string ProcessString(string value);
+        bool TryParse(string value, out string result);
+        bool Submit(ref BankAccount value);
+        string Name { get; set; }
+        int SomeOtherProperty { get; set; }
+        bool Add(int amount);
+        IBaz Baz { get; set; }
+        int GetCount();
+    }
+
+
+
+    public class MockTest
+    {
+
+        [Fact]
+        public void OrdinaryMethodCalls()
+        {
+            var mock = new Mock<IFoo>();
+
+            mock.Setup(foo => foo.DoSomething(It.IsIn("Ping", "Foo"))).Returns(false);
+            mock.Setup(foo => foo.DoSomething("Pong")).Returns(true);
+
+            Assert.False(mock.Object.DoSomething("Ping"));
+            Assert.False(mock.Object.DoSomething("Foo"));
+            Assert.True(mock.Object.DoSomething("Pong"));
+
+        }
+
+        [Fact]
+        public void ArgumentDependentMatching()
+        {
+            var mock = new Mock<IFoo>();
+
+            mock.Setup(foo => foo.Add(It.Is<int>(i => i % 2 == 0))).Returns(true);
+
+            mock.Setup(foo => foo.Add(It.IsInRange(1, 10, Range.Inclusive))).Returns(false);
+
+            mock.Setup(foo => foo.DoSomething(It.IsRegex("[a-z]+"))).Returns(false);
+
+            var result = mock.Object.DoSomething("123");
+            var result2 = mock.Object.Add(44);
+
+
+            Assert.True(result2);
+            Assert.False(result);
+
+        }
+
+
+        [Fact]
+        public void OutAndRefTest()
+        {
+            var mock = new Mock<IFoo>();
+
+            var requiredOutput = "ok";
+
+            mock.Setup(foo => foo.TryParse("ping", out requiredOutput)).Returns(true);
+
+            string result;
+
+
+            Assert.True(mock.Object.TryParse("ping", out result));
+            Assert.Equal(result, requiredOutput);
+
+            var thisShouldBeFalse = mock.Object.TryParse("pong", out result);
+
+            Console.WriteLine(result);
+
+
+            var account = new BankAccount(100, new ConsoleLog());
+
+            mock.Setup(foo => foo.Submit(ref account)).Returns(true);
+
+            var account2 = new BankAccount(100, new ConsoleLog());
+
+            Assert.Equal(true, mock.Object.Submit(ref account));
+
+            Assert.Equal(false, mock.Object.Submit(ref account2));
+        }
+
+        [Fact]
+        public void ProcessString()
+        {
+            var mock = new Mock<IFoo>();
+
+            mock.Setup(foo => foo.ProcessString(It.IsAny<string>())).Returns((string s) => s.ToLowerInvariant());
+
+            var calls = 0;
+
+            mock.Setup(foo => foo.GetCount()).Returns(() => calls).Callback(() => ++calls);
+
+            mock.Object.GetCount();
+            mock.Object.GetCount();
+
+            Assert.Equal(mock.Object.ProcessString("ABC"), "abc");
+            Assert.Equal(calls, 2);
+        }
+
+        [Fact]
+        public void TestException()
+        {
+            var mock = new Mock<IFoo>();
+
+            mock.Setup(foo => foo.DoSomething("kill")).Throws<InvalidOperationException>();
+
+            Assert.Throws<InvalidOperationException>(() => mock.Object.DoSomething("kill"));
+        }
+
+        [Fact]
+        public void PropertyTest()
+        {
+            var mock = new Mock<IFoo>();
+
+            mock.Setup(foo => foo.Name).Returns("bar");
+
+            Assert.Equal(mock.Object.Name, "bar");
+
+            mock.Setup(foo => foo.Baz.Name).Returns("Baz");
+            Assert.Equal(mock.Object.Baz.Name, "Baz");
+
+            bool setterCalled = false;
+
+            mock.SetupSet(foo =>
+            {
+                foo.Name = It.IsAny<string>();
+
+            }).Callback<string>(s =>
+            {
+                setterCalled = true;
+            });
+
+            mock.Object.Name = "def";
+
+            Assert.True(setterCalled);
+
+
+            mock.SetupProperty(foo => foo.Name);
+
+            IFoo f = mock.Object;
+
+            f.Name = "abc";
+
+            Assert.Equal(f.Name, "abc");
+        }
+
+        [Fact]
+        public void TestEvents()
+        {
+            var mock = new Mock<IAnimal>();
+
+            var doctor = new Doctor(mock.Object);
+
+            mock.Raise(animal => animal.FallsIll += null,
+                new EventArgs());
+
+            mock.Setup(animal => animal.Stumble()).Raises(animal => animal.FallsIll += null,
+                new EventArgs());
+
+            mock.Object.Stumble();
+
+            mock.Raise(animal => animal.AlienAbduction += null, 42);
+
+            Assert.Equal(doctor.TimesCured, 2);
+            Assert.Equal(doctor.AbductionObserved, 1);
+        }
+
+        [Fact]
+        public void TestCallBacks()
+        {
+            var mock = new Mock<IFoo>();
+
+            int x = 0;
+
+            mock.Setup(foo => foo.DoSomething(It.IsAny<string>())).Returns(true).Callback<string>((s) => x += s.Length);
+
+            mock.Object.DoSomething("abc");
+
+            Assert.Equal(x, 3);
+        }
+
+        [Fact]
+        public void VerificationTest()
+        {
+            var mock = new Mock<IFoo>();
+
+            var consumer = new Consumer(mock.Object);
+
+            consumer.Hello();
+
+            mock.Verify(foo => foo.DoSomething("ping"), Times.AtLeastOnce);
+            mock.Verify(foo => foo.DoSomething("pong"), Times.Never);
+
+            mock.VerifyGet(foo => foo.Name, Times.Exactly(1));
+            mock.VerifySet(foo => foo.SomeOtherProperty, Times.Exactly(1));
+        }
+
+        [Fact]
+        public void ProtectedMemberTest()
+        {
+            var mock = new Mock<Person>();
+
+            mock.Protected().SetupGet<int>("SSN").Returns(42);
+
+            mock.Protected().Setup<string>("Execute", ItExpr.IsAny<string>());
+
+        }
+    }
+}
